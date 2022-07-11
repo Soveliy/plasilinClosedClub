8e66c71bbfb0eabcd3c22be758e078e485384f49
Soveliy <nikita.elagin2012@yandex.ru>
2022-07-11 17:03:31 +0300

    added main-menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,28 @@
 
-// $(document).ready(function(){
-//     $('#marquee').marquee({
-//         duration: 15000,
-//         startVisible: true,
-//         duplicated: true,
-//         pauseOnHover:true,
-//       });
-// })
+$(document).ready(function(){
+    $('#marquee').marquee({
+        duration: 15000,
+        startVisible: true,
+        duplicated: true,
+        pauseOnHover:true,
+      });
+})
+
+$(".burger").click(function() {
+    $(".main-menu").addClass("js-active")
+})
+$(".main-menu__close,.main-menu__shadow").click(function() {
+    $(".main-menu").removeClass("js-active")
+})
+$(".js-partner").click(function(){
+    $("#callback-modal").arcticmodal({
+    //   afterOpen: function(data, el) {
+    //     $('body').css('overflow','hidden');
+    //   },
+    //   beforeClose: function(data, el) {
+    //    setTimeout(() => {
+    //     $('body').css('overflow','auto');
+    //    }, 100);
+    //   },
+    });
+  });
